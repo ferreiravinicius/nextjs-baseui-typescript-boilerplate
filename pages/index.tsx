1cfb1f976bc3c483056da7fd40b65f70c6c954dd
Dylan Babbs <babbs@uber.com>
2023-02-02 14:34:38 -0300

    upgrade baseui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Button, KIND, SHAPE, SIZE } from "baseui/button";
 import { Heading, HeadingLevel } from "baseui/heading";
 import { Cell, Grid } from "baseui/layout-grid";
-import { Paragraph3 } from "baseui/typography";
+import { ParagraphSmall } from "baseui/typography";
 import Head from "next/head";
 
 export default function Home() {
@@ -16,14 +16,14 @@ export default function Home() {
         <Cell span={12}>
           <HeadingLevel>
             <Heading>Boilerplate Next + BaseWeb </Heading>
-            <Paragraph3>
+            <ParagraphSmall>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Vestibulum fermentum velit ante, ac fringilla nulla pulvinar in.
               Aenean ut nisi mattis, lobortis purus vel, aliquet ante. In vel
               viverra lectus. Vivamus a diam faucibus, rutrum quam a, varius
               felis. Sed pellentesque sodales libero commodo vestibulum.
               Phasellus convallis gravida tempor. Sed ut bibendum nisl.
-            </Paragraph3>
+            </ParagraphSmall>
             <Button
               onClick={() => alert("click")}
               kind={KIND.primary}
